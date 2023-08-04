095dc6de98f08a24a3d51b67769c7524c8abc881
Smartycool <140168331+Smartycool@users.noreply.github.com>
2023-08-04 17:01:43 +0530

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -12,7 +12,7 @@ class AnonXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍",
+            "𓂃⃪ํֶ𝙎𝙈𝘼𝙍𝙏𝙔 𝙑𝙁™ ̶꯭❍͢͠ᴘ𓂃[🇮🇳]",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,

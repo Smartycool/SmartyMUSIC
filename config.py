a5cde4d575d40aa50227b546dedb62361361090a
Smartycool <140168331+Smartycool@users.noreply.github.com>
2023-08-04 15:42:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,19 +14,19 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", ": ™°‌ 🫧 𝘽𝙡𝙤𝙨𝙨𝙤𝙢𓃭 ⃟ᴍᴜsɪᴄ⛦⃕͜༆")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", ": ˹Mᴀғɪᴀ ✘ Mᴜ𝘀ɪᴄ˼🫧")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5059737154").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6164846609").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/PRADHAN474/OXYMUSIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Smartycool/SmartyMUSIC")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BWANDARLOK")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/PRADHAN474")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THE_MAFIA_CLAN")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/smarty_boy11")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
